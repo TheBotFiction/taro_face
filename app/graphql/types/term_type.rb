9d0585994d74f2d330f50d77a8cd76c305a25dda
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-08-24 15:56:16 +0900

    feat(Loader): able to load with 'limit' and 'order'

--- app/graphql/types/term_type.rb
+++ app/graphql/types/term_type.rb
@@ -6,11 +6,21 @@ module Types
     field :term, String, null: true
     field :reading, String, null: true
     field :meaning, String, null: true
-    field :sample_phrases, [SamplePhraseType], null: true,
-      description: "List of sample phrase associated to term"
+    field :sample_phrases, [SamplePhraseType], null: true do
+      description  "List of sample phrase associated to term"
+      argument :limit, Integer, required: false,
+        description: "Limit the phrase returned. Default is 5"
+      argument :order, String, required: false,
+        description: "DESC or ASC. Default is ASC"
+    end
 
-    def sample_phrases
-      Loaders::AssociationLoader.for(Term, :sample_phrases).load(object)
+    def sample_phrases(limit: 5, order: nil)
+      Loaders::AssociationLoader.for(
+        Term,
+        :sample_phrases,
+        limit: limit,
+        order: order
+      ).load(object)
     end
   end
 end
