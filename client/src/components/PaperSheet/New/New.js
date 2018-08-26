e83763f33d0682a22df5d5a318d5d69a07a7e650
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-08-26 17:02:59 +0900

    fix(client): minor fixes

--- client/src/components/PaperSheet/New/New.js
+++ client/src/components/PaperSheet/New/New.js
@@ -5,7 +5,6 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Paper from '@material-ui/core/Paper'
 import Grid from '@material-ui/core/Grid'
 import Button from '@material-ui/core/Button'
 import CloudUploadIcon from '@material-ui/icons/CloudUpload'
