f3ffb022550736b75e74553a6d8b3a207d611dd5
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-08-30 00:42:10 +0900

    fix(client@PaperSheet/New): ui for chosen questions

--- client/src/components/PaperSheet/New/Chosen/ChosenQuestionItem.js
+++ client/src/components/PaperSheet/New/Chosen/ChosenQuestionItem.js
@@ -6,10 +6,7 @@ import type { Node } from 'react'
 import type { TermType, PaperSheetType$ChosenQuestion } from 'types'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Typography from '@material-ui/core/Typography'
-import Avatar from '@material-ui/core/Avatar'
-import Chip from '@material-ui/core/Chip'
+import { Grid, Typography, Avatar, Chip, Paper } from '@material-ui/core'
 import deepPurple from '@material-ui/core/colors/deepPurple'
 
 type Props = {
@@ -18,33 +15,35 @@ type Props = {
 
 const ChosenQuestionItem: Function = ({classes, term, answers, samplePhrase}: Props): Node => (
   <Grid item container direction="column">
-    <Grid item>
-      <Typography variant="headline" component="h3">
-        {samplePhrase.phrase}
-      </Typography>
-    </Grid>
-    <Grid
-      item
-      container
-      spacing={40}
-      justify="space-around"
-    >
-      {answers.map((answer: TermType, index: number): Node => (
-        <Grid item xs key={index}>
-          <Chip
-            color={answer.id === term.id ? 'secondary' : 'default'}
-            avatar={
-              <Avatar className={classes.avatar}>
-                {String.fromCharCode(65 + index)}
-              </Avatar>
-            }
-            label={answer.term}
-            clickable
-            onClick={() => alert('Not now')}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Paper className={classes.paper}>
+      <Grid item>
+        <Typography variant="headline" component="h3">
+          {samplePhrase.phrase}
+        </Typography>
+      </Grid>
+      <Grid
+        item
+        container
+        spacing={40}
+        justify="space-around"
+      >
+        {answers.map((answer: TermType, index: number): Node => (
+          <Grid item xs key={index}>
+            <Chip
+              color={answer.id === term.id ? 'secondary' : 'default'}
+              avatar={
+                <Avatar className={classes.avatar}>
+                  {String.fromCharCode(65 + index)}
+                </Avatar>
+              }
+              label={answer.term}
+              clickable
+              onClick={() => alert('Not now')}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
   </Grid>
 )
 
@@ -55,11 +54,16 @@ ChosenQuestionItem.propTypes = {
   samplePhrase: PropTypes.object.isRequired
 }
 
-const styles: Object = {
+const styles: Function = (theme: any) => ({
   avatar: {
     color: '#fff',
     backgroundColor: deepPurple[500]
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
   }
-}
+})
 
 export default withStyles(styles)(ChosenQuestionItem)
--- client/src/components/PaperSheet/New/Preview/Preview.js
+++ client/src/components/PaperSheet/New/Preview/Preview.js
@@ -6,10 +6,8 @@ import React, { Component } from 'react'
 import type { PaperSheetType$ChosenQuestion } from 'types'
 import PropTypes from 'prop-types'
 import { withStyles } from '@material-ui/core/styles'
-import Grid from '@material-ui/core/Grid'
-import Button from '@material-ui/core/Button'
+import { Grid, Button } from '@material-ui/core'
 import CloudUploadIcon from '@material-ui/icons/CloudUpload'
-import Paper from '@material-ui/core/Paper'
 import ChosenQuestionItem from '../Chosen/ChosenQuestionItem'
 
 type Props = {
@@ -17,7 +15,7 @@ type Props = {
   onChooseQuestion: Function
 } & PaperSheetType$ChosenQuestion
 
-class PreviewComponent extends Component<Props, *> {
+class PreviewComponent extends Component<Props, null> {
   static propTypes = {
     term: PropTypes.object.isRequired,
     answers: PropTypes.array.isRequired,
@@ -46,7 +44,7 @@ class PreviewComponent extends Component<Props, *> {
     const { term, answers, samplePhrase, classes }: Props = this.props
 
     return (
-      <Paper className={classes.root}>
+      <div className={classes.root}>
         <Grid container spacing={16} direction="column">
           <ChosenQuestionItem term={term} answers={answers} samplePhrase={samplePhrase} />
           <Grid item>
@@ -62,7 +60,7 @@ class PreviewComponent extends Component<Props, *> {
             </Button>
           </Grid>
         </Grid>
-      </Paper>
+      </div>
     )
   }
 }
