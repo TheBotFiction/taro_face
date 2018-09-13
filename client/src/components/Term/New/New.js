d3743a36d3a803ce6cf75bd80b422b512f178862
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-09-14 02:41:08 +0900

    feat(client@Term/New): animations for analyze terms

--- client/src/components/Term/New/New.js
+++ client/src/components/Term/New/New.js
@@ -4,7 +4,9 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import withStyles from '@material-ui/core/styles/withStyles'
-import { CssBaseline, Button } from '@material-ui/core'
+import { Grid, CssBaseline, Button, Divider, Collapse, Slide } from '@material-ui/core'
+import CloudUploadIcon from '@material-ui/icons/CloudUpload'
+import FastfoodIcon from '@material-ui/icons/Fastfood'
 import { Form, Field } from 'react-final-form'
 import Layout from 'components/Layout'
 import TextField from 'components/UIKit/TextField'
@@ -18,10 +20,11 @@ type Props = {
 }
 
 const paralax: Object = {
-  image: require('assets/img/bg7.jpg')
+  image: require('assets/img/bg7.jpg'),
+  title: 'Create new Terms'
 }
 
-export class TermNewComponent extends Component<Props> {
+export class TermNewComponent extends Component<Props, *> {
   static propTypes = {
     classes: PropTypes.object.isRequired,
     terms: PropTypes.arrayOf(PropTypes.object),
@@ -35,40 +38,104 @@ export class TermNewComponent extends Component<Props> {
     onAnalyze: () => {}
   }
 
-  render() {
+  togglePhraseInput: Function
+
+  constructor (props: Props) {
+    super(props)
+    this.togglePhraseInput = this.togglePhraseInput.bind(this)
+  }
+
+  state = {
+    showPhraseInput: false
+  }
+
+  togglePhraseInput (e: any) {
+    const { showPhraseInput } = this.state
+    if (showPhraseInput) return
+    e.preventDefault()
+    this.setState({showPhraseInput: !this.state.showPhraseInput})
+  }
+
+  render () {
     const { classes, onSubmit, onAnalyze, terms } = this.props
+    const { showPhraseInput } = this.state
     return (
       <Layout paralax={paralax}>
         <CssBaseline />
         <main className={classes.root}>
           <Form
             onSubmit={onAnalyze}
-            render={({handleSubmit}) => (
+            render={({ handleSubmit, pristine, submitting, invalid }) => (
               <form onSubmit={handleSubmit}>
-                <Field
-                  label="Sample phrase"
-                  name="samplePhrase"
-                  margin="normal"
-                  multiline
-                  rows="4"
-                  fullWidth
-                  helperText="Give a sample paragraph. Taro will help you to pick terms"
-                  component={TextField}
-                />
-                <Button type="submit">Analyze</Button>
+                <Collapse in={showPhraseInput}>
+                  <Field
+                    label="Sample phrase"
+                    name="samplePhrase"
+                    margin="normal"
+                    required
+                    multiline
+                    rows="4"
+                    fullWidth
+                    helperText="Give a sample paragraph. Taro will help you to pick terms"
+                    component={TextField}
+                  />
+                </Collapse>
+                <Grid container>
+                  <Grid item xs={8} />
+                  <Grid item xs={4} className={classes.buttonContainer}>
+                    <Button
+                      variant="outlined"
+                      color="primary"
+                      size="small"
+                      className={classes.button}
+                      type={showPhraseInput ? 'submit' : 'button'}
+                      onClick={this.togglePhraseInput}
+                      disabled={submitting || invalid}
+                    >
+                      Analyze Phrase to Terms
+                      <FastfoodIcon className={classes.rightIcon} />
+                    </Button>
+                  </Grid>
+                </Grid>
               </form>
             )}
           />
+          <Divider className={classes.divider} />
           <Form
             onSubmit={onSubmit}
             validate={() => {}}
             initialValues={{ term: terms }}
-            render={({ handleSubmit, pristine, submitting, invalid, submitErrors }) => (
+            render={({ handleSubmit, pristine, submitting, invalid }) => (
               <form onSubmit={handleSubmit}>
-                {terms.map((_term, index) => (
-                  <Term index={index} key={index} />
+                {terms.map((term, index) => (
+                  <Slide
+                    direction="up"
+                    in={!!term}
+                    style={{ transitionDelay: 50 }}
+                    mountOnEnter
+                    unmountOnExit
+                    key={index}
+                  >
+                    <Term index={index} />
+                  </Slide>
                 ))}
-                <Button type="submit">Create</Button>
+                <Divider className={classes.divider} />
+                <Grid container>
+                  <Grid item xs={8} />
+                  <Grid item xs={4} className={classes.buttonContainer}>
+                    <Button
+                      variant="contained"
+                      color="secondary"
+                      size="small"
+                      type="submit"
+                      disabled={terms.length <= 0 || submitting}
+                      className={classes.button}
+                    >
+                      Create Terms
+                      <CloudUploadIcon className={classes.rightIcon} />
+                    </Button>
+                  </Grid>
+                </Grid>
               </form>
             )}
           />
@@ -85,6 +152,16 @@ const styles = (theme: Object): Object => ({
     maxWidth: theme.breakpoints.values.md,
     marginLeft: 'auto',
     marginRight: 'auto'
+  },
+  rightIcon: {
+    marginLeft: theme.spacing.unit
+  },
+  divider: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3
+  },
+  buttonContainer: {
+    textAlign: 'right'
   }
 })
 
