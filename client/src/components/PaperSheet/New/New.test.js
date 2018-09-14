f424a9e1f95ba776e1cf32a09c0b31723991c8cd
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-09-14 12:44:35 +0900

    test(client): PaperSheetNew{Container,Component} with server mocks

--- client/src/components/PaperSheet/New/New.test.js
+++ client/src/components/PaperSheet/New/New.test.js
@@ -5,17 +5,32 @@ import { shallow } from 'enzyme'
 import New from './New'
 
 const noop = () => {}
+const EmptyNode = <span />
 
-describe('component Term/Show', () => {
+describe('component Term/New', () => {
   it('renders without crash', () => {
     shallow(
-      <New loadOptions={noop} onSelectTerm={noop} previewSlot={<span />} />
+      <New
+        loadOptions={noop}
+        onSelectTerm={noop}
+        onChooseQuestion={noop}
+        onSubmit={noop}
+        previewSlot={EmptyNode}
+        chosenSlot={EmptyNode}
+      />
     )
   })
 
   it('renders as expected', () => {
     const component = TestRenderer.create(
-      <New loadOptions={noop} onSelectTerm={noop} previewSlot={<span />} />
+      <New
+        loadOptions={noop}
+        onSelectTerm={noop}
+        onChooseQuestion={noop}
+        onSubmit={noop}
+        previewSlot={EmptyNode}
+        chosenSlot={EmptyNode}
+      />
     )
     const json = component.toJSON()
     expect(json).toMatchSnapshot()
