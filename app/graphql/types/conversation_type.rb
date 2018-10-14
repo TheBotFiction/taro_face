dd27ebd32befcf6e69c02a831d9d33399207eb69
p.hoai.le <yeuem1vannam@users.noreply.github.com>
2018-10-14 15:48:45 +0900

    feat(server): add description to ConversationType

--- app/graphql/types/conversation_type.rb
+++ app/graphql/types/conversation_type.rb
@@ -4,6 +4,7 @@ module Types
   class ConversationType < BaseObject
     field :id, ID, null: false
     field :title, String, null: true
+    field :description, String, null: true
     field :messages, [MessageType], null: true,
       description: "List of messages in the conversation"
   end
